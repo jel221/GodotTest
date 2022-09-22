f8618aac6968d7a26f84a08542d44bb753954e9c
Jacob Leigh <jacoblaurenceleigh@berkeley.edu>
2022-09-22 14:11:22 -0700

    cam basic

--- CamMov.gd
+++ CamMov.gd
@@ -1,18 +1,21 @@
 extends Spatial
 
-var cam_view = 0
+var angle_h = 0
+var angle_v = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$xy/ClippedCamera.add_exception(get_parent())
+	$xy/z/ClippedCamera.add_exception(get_parent())
 	
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		cam_view -= event.relative.x
+		angle_h -= event.relative.x
+		angle_v += event.relative.y
 		
 func _physics_process(delta):
-	$xy.rotation_degrees.y = cam_view
+	$xy.rotation_degrees.y = angle_h
+	$xy/z.rotation_degrees.x = angle_v
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
