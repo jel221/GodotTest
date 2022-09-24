d87a31007ad3d48a4fb8ba3be5d828e6145edf42
Jacob Leigh <jacoblaurenceleigh@berkeley.edu>
2022-09-24 00:04:30 -0700

    mouse rotation lock

--- CamMov.gd
+++ CamMov.gd
@@ -3,19 +3,31 @@ extends Spatial
 var angle_h = 0
 var angle_v = 0
 
+const angle_v_MIN = -75
+const angle_v_MAX = 75
+
+onready var camera = $xy/z/ClippedCamera
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$xy/z/ClippedCamera.add_exception(get_parent())
+	camera.add_exception(get_parent())
 	
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		angle_h -= event.relative.x
 		angle_v += event.relative.y
+	# This function is copied off the Internet, so I'm not sure how well it works.
+	# "Good programmers write good code, great programmers steal great code."
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_WHEEL_UP:
+			camera.fov += 2
+		elif event.button_index == BUTTON_WHEEL_DOWN:
+			camera.fov -= 2
 		
 func _physics_process(delta):
+	angle_v = clamp(angle_v, angle_v_MIN, angle_v_MAX)
 	$xy.rotation_degrees.y = angle_h
 	$xy/z.rotation_degrees.x = angle_v
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	
--- Movement.gd
+++ Movement.gd
@@ -1,26 +1,23 @@
-extends RigidBody
+extends KinematicBody
 
 
 onready var camera = $Spatial/xy/z/ClippedCamera
-onready var start_position = translation
+var dir = Vector3.FORWARD
+const MOVE_SPEED = 6 # YOU CAN MODIFY AND PLAY AROUND WITH THIS
 
 func _physics_process(_delta):
-	if Input.is_action_just_pressed("exit"):
-		get_tree().quit()
-	if Input.is_action_just_pressed("reset_position"):
-		translation = start_position
-		return
 
-	var dir = Vector3()
+	var xy = camera.global_transform.basis.get_euler().y
+	
+	dir = Vector3()
 	dir.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	dir.z = Input.get_action_strength("move_back") - Input.get_action_strength("move_forward")
-
+	dir = dir.rotated(Vector3.UP, xy)
+	
+	move_and_slide(dir * MOVE_SPEED, Vector3.UP)
 	# Get the camera's transform basis, but remove the X rotation such
 	# that the Y axis is up and Z is horizontal.
 	var cam_basis = camera.global_transform.basis
 	var basis = cam_basis.rotated(cam_basis.x, -cam_basis.get_euler().x)
 	dir = basis.xform(dir)
-
-	apply_central_impulse(dir.normalized() / 10)
-
 
